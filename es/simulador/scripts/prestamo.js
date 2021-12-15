99b52a463729a7a25de1b688a1a1d122f7edf086
Pas-2021 <90237663+Pas-2021@users.noreply.github.com>
2021-12-15 16:58:05 -0500

    Add files via upload

--- es/simulador/scripts/prestamo.js
+++ es/simulador/scripts/prestamo.js
@@ -1,25 +1,23 @@
 function gen_table(){
     document.getElementById("tab").innerHTML="";
+
     let n=Number(document.getElementById("capital").value);
+
     let n2=Number(document.getElementById("couta").value);
 
-
-
-
+    let n4=Number(document.getElementById("duracion-meses").value);
     
-    let n3=Number(document.getElementById("interes").value);
-
- 
+    let n3=Number(document.getElementById("interes").value); 
 
 
     if(n>0){   
         for(i=1;i<=n2;i++){
             ca=n/n2;
-            d1=ca.toFixed(2);
-            i2=((n*n3)/100)/n2;
-            d2=i2.toFixed(2);
+            d1=ca.toFixed();
+            i2=((n*(n3*n4))/100)/n2;
+            d2=i2.toFixed();
             r=ca+i2;
-            d3=r.toFixed(2);
+            d3=r.toFixed();
             document.getElementById("tab").innerHTML=document.getElementById("tab").innerHTML+
                     `<tr>
                         <td> ${i}</td>
@@ -27,11 +25,11 @@ function gen_table(){
                         <td> ${d3}</td>
                     </tr>`;
         }
-        n1=n.toFixed(2);
+        n1=n.toFixed();
         t_i=i2*n2;
-        d4=t_i.toFixed(2);
+        d4=t_i.toFixed();
         t_p=r*n2;
-        d5=t_p.toFixed(2);
+        d5=t_p.toFixed();
         //document.getElementById("t1").innerHTML=n1;
         //document.getElementById("t2").innerHTML=d4;
         document.getElementById("t3").innerHTML=d5;        
